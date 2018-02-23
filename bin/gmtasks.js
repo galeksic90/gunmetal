ed1232a63e10f97b49e3e8579a1002e5b8e3aaaa
Goran Aleksić <galeksic90@gmail.com>
2018-02-23 17:42:21 +0100

    Version bump

--- bin/gmtasks.js
+++ bin/gmtasks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-process.stdout.write("gunmetal v1.2.1\r");
+process.stdout.write("gunmetal v1.2.2\r");
 process.env.GULP_PROJECTS_DIR = process.cwd();
 process.chdir(require('gunmetal').dirname);
 require('gulp-cli')();
